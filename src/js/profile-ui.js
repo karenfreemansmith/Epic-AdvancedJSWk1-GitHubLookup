cfe66b6208098fe46bc4b4bc89f9949194794e7c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-10-14 11:45:52 -0700

    add formatting for user profile and repo list

--- src/js/profile-ui.js
+++ src/js/profile-ui.js
@@ -1,7 +1,16 @@
 var GitHubUser = require('./../src/js/profile.js').getRepos;
 
 var displayUser = function(response) {
-  $(".showUser").text(response.name);
+  $("#avatar").append("<img src='"+ response.avatar_url+"' class='img-responsive img-thumbnail' alt='GitHub Avatar'>");
+  $(".showUser").append("<h3>" + response.name + ":<br> "+ response.public_repos + " Repositories</h3>");
+  $(".showUser").append("<p>" + response.bio + "</p>");
+  console.log(response);
+};
+
+var displayRepos = function(response) {
+  response.forEach(function(repo) {
+    $("#repolist").append("<h3><a href='"+repo.html_url+"' target='_blank'>"+repo.name+"</a></h3><p>"+repo.description+"</p>")
+  });
 };
 
 $(document).ready(function() {
@@ -10,6 +19,7 @@ $(document).ready(function() {
   $('#getUser').click(function() {
     var username = $('#username').val();
     $('#username').val("");
-    currentGitHubUser.getRepos(username, displayUser);
+    currentGitHubUser.getUser(username, displayUser);
+    currentGitHubUser.getRepos(username, displayRepos);
   });
 });
--- src/js/profile.js
+++ src/js/profile.js
@@ -3,10 +3,19 @@ var apiKey = require('./../../.env').apiKey;
 function GitHubUser() {
 }
 
-GitHubUser.prototype.getRepos = function(username, displayUser){
+GitHubUser.prototype.getUser = function(username, displayUser){
   $.get('https://api.github.com/users/'+ username +'?access_token=' + apiKey)
     .then(function(response){
       displayUser(response);
+    })
+    .fail(function(error){
+    });
+  };
+
+GitHubUser.prototype.getRepos = function(username, displayRepos){
+  $.get('https://api.github.com/users/'+ username +'/repos?access_token=' + apiKey)
+    .then(function(response){
+      displayRepos(response);
     })
     .fail(function(error){
     });
