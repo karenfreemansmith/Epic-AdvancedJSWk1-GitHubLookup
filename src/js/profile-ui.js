d7aa22dfdc4bc325d27529e9678f52b212432a78
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-10-14 14:05:46 -0700

    add todos and increase followers and repos displayed

--- src/js/profile-ui.js
+++ src/js/profile-ui.js
@@ -4,23 +4,23 @@ var displayUser = function(response) {
   $(".showUser").text("");
   $(".showUser").append("<img src='"+ response.avatar_url+"' class='img-responsive img-thumbnail' alt='GitHub Avatar'>");
   $(".showUser").append("<h3>" + response.name + ":<br> "+ response.public_repos + " Repositories</h3>");
-  $(".showUser").append("<p>" + response.bio + "</p>");
+  $(".showUser").append("<p>" + response.bio + " (" + response.email + ")</p>");
   console.log(response);
 };
 
 var displayFavorites = function(response) {
-  $(".showUser").append("<ul class='favorites'>");
+  $(".showUser").append("<div class='favorites row'>");
   response.forEach(function(fav) {
-    $(".favorites").append("<li>"+fav.login+"</li>");
+    $(".favorites").append("<div class='col-sm-4'>"+fav.login+"</div>");
   });
-  $(".showUser").append("</ul>");
+  $(".showUser").append("</div");
   console.log(response);
 };
 
 var displayRepos = function(response) {
   $(".showRepos").text("");
   response.forEach(function(repo) {
-    $(".showRepos").append("<h3><a href='"+repo.html_url+"' target='_blank'>"+repo.name+"</a></h3><p>"+repo.description+"</p>")
+    $(".showRepos").append("<div class='col-sm-6'><h3><a href='"+repo.html_url+"' target='_blank'>"+repo.name.substring(0,24)+"</a></h3><p>"+repo.description+"</p></div>")
   });
   console.log(response);
 };
--- src/js/profile.js
+++ src/js/profile.js
@@ -1,4 +1,5 @@
 var apiKey = require('./../../.env').apiKey;
+// TODO: add logic for users when key is not available
 
 function GitHubUser() {
 }
@@ -9,12 +10,15 @@ GitHubUser.prototype.getUser = function(username, displayUser){
       displayUser(response);
     })
     .fail(function(error){
+      // TODO: add some logic to update UI when a user is not found
     });
   };
 
 GitHubUser.prototype.getFavorites = function(username, displayFavorites){
-  $.get('https://api.github.com/users/'+ username +'/following?access_token=' + apiKey)
+  $.get('https://api.github.com/users/'+ username +'/following?per_page=99&access_token=' + apiKey)
     .then(function(response){
+      // TODO: add users to an array and run pagination until there are no more users
+      // TODO: sort users by name (case insensitive)
       displayFavorites(response);
     })
     .fail(function(error){
@@ -22,8 +26,10 @@ GitHubUser.prototype.getFavorites = function(username, displayFavorites){
   };
 
 GitHubUser.prototype.getRepos = function(username, displayRepos){
-  $.get('https://api.github.com/users/'+ username +'/repos?access_token=' + apiKey)
+  $.get('https://api.github.com/users/'+ username +'/repos?sort=pushed&per_page=99&access_token=' + apiKey)
     .then(function(response){
+      // TODO: add repos to an array and run pagination until there are no more repos
+      // TODO: sort repos by number of commits? size? Something that might indicate "best" repost at the top of the list better than just date order
       displayRepos(response);
     })
     .fail(function(error){
