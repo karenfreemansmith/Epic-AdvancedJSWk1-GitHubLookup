50b3dd12e7d10232e9944fa52eda2da6fd2f13fc
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-01-02 12:23:47 -0800

    fix problem with API key

--- README.md
+++ README.md
@@ -1,35 +1,29 @@
 # GitHub Profiles
 Version 0.0.1: October 14, 2016
-
 by [Karen Freeman-Smith](https://github.com/karenfreemansmith)
+
+![screenshot of project running](screenshot.png)
 
 ## Description
 Epicodus JavaScript week 1 final project to build a GitHub user page using the GitHub API.
 
-![screenshot of project running](screenshot.png)
+### Technologies Used
+HTML, CSS, JavaScript, Node.js, NPM, Gulp, Browserify, Uglify, Concat, Bower, and more!
 
 ### Specifications
 Show github profile with repositories for a given username.
-
-## Support & Contact
-For questions, concerns, or suggestions please email karenfreemansmith@gmail.com
 
 ## Setup/Installation
 * Clone directory
 * Run 'npm install'
 * Run 'bower install'
-* Obtain an API key from [GitHub](https://github.com)
-  * *Visit the Settings area of your GitHub account, select Personal Access Tokens from the sidebar, and hit Generate New Token.*
-* Create an .env file in the main directory with this line of code:
-  * exports.apiKey = "(_insert your new API key here_)";
 * Type 'gulp serve'
 
-## Known Issues
-* requires APIKey from GitHub to run
-* gulp deploy fails on Epicodus machines (may be from typo on password, but has error in npm file now)
+## Support & Contact
+For questions, concerns, or suggestions please email karenfreemansmith@gmail.com
 
-## Technologies Used
-HTML, CSS, JavaScript, Node.js, NPM, Gulp, Browserify, Uglify, Concat, Bower, and more!
+## Known Issues
+* n/a
 
 ## Legal
 *Licensed under the GNU General Public License v3.0*
--- src/js/profile.js
+++ src/js/profile.js
@@ -1,11 +1,11 @@
 // TODO: add logic for users when key is not available
-var apiKey = require('./../../.env').apiKey;
+//var apiKey = require('./../../.env').apiKey;
 
 function GitHubUser() {
 }
 
 GitHubUser.prototype.getUser = function(username, displayUser, displayUserNotFound) {
-  $.get('https://api.github.com/users/'+ username +'?access_token=' + apiKey)
+  $.get('https://api.github.com/users/'+ username)
     .then(function(response){
       displayUser(response);
     })
@@ -16,7 +16,7 @@ GitHubUser.prototype.getUser = function(username, displayUser, displayUserNotFou
 
 GitHubUser.prototype.getFavorites = function(username, displayFavorites) {
   var favorites=[];
-  $.get('https://api.github.com/users/'+ username +'/following?per_page=99&access_token=' + apiKey)
+  $.get('https://api.github.com/users/'+ username +'/following?per_page=99')
     .then(function(response){
       response.forEach(function(fav) {
         favorites.push(fav.login);
@@ -41,12 +41,12 @@ GitHubUser.prototype.getFavorites = function(username, displayFavorites) {
 
 GitHubUser.prototype.getRepos = function(username, displayRepos) {
   var bigresponse=[];
-  $.get('https://api.github.com/users/'+ username +'/repos?sort=pushed&per_page=100&access_token=' + apiKey)
+  $.get('https://api.github.com/users/'+ username +'/repos?sort=pushed&per_page=100')
     .then(function(response){
       response.forEach(function(repo) {
         bigresponse.push(repo);
       });
-      $.get('https://api.github.com/users/'+ username +'/repos?sort=pushed&page=2&per_page=100&access_token=' + apiKey)
+      $.get('https://api.github.com/users/'+ username +'/repos?sort=pushed&page=2&per_page=100')
         .then(function(response){
           response.forEach(function(repo) {
             bigresponse.push(repo);
